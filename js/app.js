1e6e1924f7e50f87c540e480a69ad8e084b025bd
Melissa Morin <melissa.morinmangaillou@gmail.com>
2020-03-13 13:21:18 -0400

    url thumbnail

--- js/app.js
+++ js/app.js
@@ -6,10 +6,32 @@ fetch(url).then(resp => {
 
         for (let post of posts) {
             const title = post.data.title
-            const thumbail = post.data.thumbail
+            const thumbnail_src = post.data.thumbnail
             const url_big_img = post.data.url
+            const container = document.querySelector(".container-scroll")
+            const big_img = document.querySelector(".big-img")
 
-            
+            let thumbnail = document.createElement("img")
+            thumbnail.setAttribute("src", thumbnail_src)
+
+            let template = document.createElement("div")
+            template.classList.add('template')
+
+            let overlay = document.createElement("div")
+            overlay.classList.add('overlay')
+
+            container.appendChild(template)
+            template.appendChild(overlay)
+            template.appendChild(thumbnail)
+
+            template.addEventListener("click", () => {
+                big_img.setAttribute("src", url_big_img)
+                // console.log("allo")
+            })
+
+
+
+
 
 
         }
