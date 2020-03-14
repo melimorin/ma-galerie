39b6adbd2b8b6b25f09f5fd5b5ab02986d41aecd
melissa morin <melissa.morinmangaillou@gmail.com>
2020-03-14 14:47:05 -0400

    click and scroll

--- js/app.js
+++ js/app.js
@@ -3,9 +3,13 @@ const url = "https://www.reddit.com/r/DigitalArt.json"
 fetch(url).then(resp => {
     resp.json().then(api => {
         const posts = api.data.children
+        
+        const container = document.querySelector(".container-scroll")
+        const big_img = document.querySelector(".big-img")
+        const big_img_left = document.querySelector(".big-img-left")
+        const big_img_right = document.querySelector(".big-img-right")
+        container.style.left = "0px"
 
-        // for (let post of posts) {
-            
         for (let i = 0; i < posts.length; i++) {
             const post = posts[i]
 
@@ -21,10 +25,6 @@ fetch(url).then(resp => {
             const title = post.data.title
             const thumbnail_src = post.data.thumbnail
             const url_big_img = post.data.url
-            const container = document.querySelector(".container-scroll")
-            const big_img = document.querySelector(".big-img")
-            const big_img_left = document.querySelector(".big-img-left")
-            const big_img_right = document.querySelector(".big-img-right")
 
             let thumbnail = document.createElement("img")
             thumbnail.setAttribute("src", thumbnail_src)
@@ -52,5 +52,28 @@ fetch(url).then(resp => {
 
 
         }
+
+        let drag = false
+        let last_position = null
+
+        container.addEventListener("mousedown", e => {
+            drag = true
+            console.log("licorne")
+        })
+        document.addEventListener("mouseup", e => {
+            drag = false
+            console.log("licorne-rose")
+        })
+        container.addEventListener("mousemove", e => {
+            if (drag == true) {
+                let position = e.clientX
+                let offset = last_position - position
+                let current_position = parseFloat(container.style.left)
+                let final_position = current_position - offset
+                container.style.left = final_position + "px"
+            }
+
+            last_position = e.clientX
+        })
     })
 })
